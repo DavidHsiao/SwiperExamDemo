33c6975221b617afb55a2838b6b9115416ff446a
David Hsiao <davidhsiao@cybersoft4u.com>
2021-03-23 18:36:28 +0800

    * 加入Tab Layout的控制

--- app/src/main/java/android/example/com/swiperexamdemo/wallet/WalletFragment.kt
+++ app/src/main/java/android/example/com/swiperexamdemo/wallet/WalletFragment.kt
@@ -1,5 +1,6 @@
 package android.example.com.swiperexamdemo.wallet
 
+import android.example.com.swiperexamdemo.R
 import android.example.com.swiperexamdemo.databinding.FragmentWalletBinding
 import android.example.com.swiperexamdemo.wallet.subFragVoucher.WalletVoucherFragment
 import android.example.com.swiperexamdemo.wallet.subFragWallet.WalletCoinFragment
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.tabs.TabLayoutMediator
 
 class WalletFragment : Fragment() {
     override fun onCreateView(
@@ -30,6 +32,17 @@ class WalletFragment : Fragment() {
         binding.lifecycleOwner = this
 
         binding.viewPager2.adapter = WalletFragmentPagerAdapter(createFragList(), this)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            when (position) {
+                0 -> tab.setText(R.string.coin_record)
+                1 -> tab.text = getString(R.string.voucher)
+//                1 -> tab.text = String.format(getString(R.string.voucher), data.voucherList.size)
+                else -> {
+                }
+            }
+        }.attach()
+
 
         return binding.root
     }
